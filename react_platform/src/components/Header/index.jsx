efcf09e1b4e8fccc6759b3b699a0d15d04efc03d
Jhones <jhonesaly@gmail.com>
2023-11-22 11:56:17 -0300

    feat: separação logado

--- react_platform/src/components/Header/index.jsx
+++ react_platform/src/components/Header/index.jsx
@@ -3,10 +3,15 @@ import logo from '../../assets/logo-name.png'
 
 import { Container, Logo } from './styles';
 
-const Header = () => {
+const Header = ({logado}) => {
     return (
     <Container>
             <Logo src={logo} alt="Logo da plataforma"/>
+            {logado ? (<>
+                <button>Sair</button>
+            </>) : (<>
+                <button>Entrar</button>
+            </>)}
     </Container>
     )
 }
