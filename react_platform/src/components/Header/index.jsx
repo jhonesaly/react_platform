9fe4f0acbfa5f3e0a80b50dea9ba275950df51f3
Jhones <jhonesaly@gmail.com>
2023-11-22 11:47:58 -0300

    feat: logo na header

--- react_platform/src/components/Header/index.jsx
+++ react_platform/src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from '../../assets/logo-name.png'
 
-import { Container } from './styles';
+import { Container, Logo } from './styles';
 
 const Header = () => {
     return (
     <Container>
-
+            <Logo src={logo} alt="Logo da plataforma"/>
     </Container>
     )
 }
--- react_platform/src/components/Header/styles.js
+++ react_platform/src/components/Header/styles.js
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100%;
-    height: 47px;
+    height: 60px;
     background-color: #000;
-
+`
+export const Logo = styled.img`
+  max-width: 100%;
+  height: 60px;
+  margin: 0 40px;   
 `
