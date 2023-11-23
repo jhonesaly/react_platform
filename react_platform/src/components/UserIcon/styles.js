0ed205e54f5caf7f39c011e88ce7a31ed4bf6960
Jhones <jhonesaly@gmail.com>
2023-11-23 12:52:44 -0300

    feat: user icon style

---
+++ react_platform/src/components/UserIcon/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const UserIconContainer = styled.img`
+    width: 30px;
+    height: 30px;
+    border-radius: 22px;
+    border: 3px solid #fff;
+`
