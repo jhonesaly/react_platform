01c1a1ba19cb7806ebc998f682410df9579775b7
Jhones <jhonesaly@gmail.com>
2023-11-23 11:27:48 -0300

    feat: button style

--- react_platform/src/components/Button/styles.js
+++ react_platform/src/components/Button/styles.js
@@ -1,5 +1,16 @@
 import styled from 'styled-components';
 
 export const ButtonContainer = styled.button`
-    
+    background: #777;
+    border-radius: 22px;
+    color: #FFF;
+    width: 100px;
+    margin: 10px 15px;
+    margin-left: auto;
+    cursor: pointer;
+    transition: background 0.3s ease;
+
+    &:hover {
+        background: #999; /* Altera a cor de fundo ao passar o mouse por cima */
+    }
 `
--- react_platform/src/components/Header/styles.js
+++ react_platform/src/components/Header/styles.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
     width: 100%;
     height: 60px;
     background-color: #000;
+    display: flex;
 `
 export const Logo = styled.img`
   max-width: 100%;
