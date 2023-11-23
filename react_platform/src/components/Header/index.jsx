a026d1ff4a073ef524f3e39ce36453f1d10066be
Jhones <jhonesaly@gmail.com>
2023-11-23 12:57:19 -0300

    feat: user icon na header

--- react_platform/src/components/Header/index.jsx
+++ react_platform/src/components/Header/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from '../../assets/logo-name.png';
+import usericon from '../../assets/anon_icon.jpg';
 
 import { Navigator } from '../Navigator';
 import { Button } from '../Button';
+import { UserIcon } from '../UserIcon';
 
 import { Container, Logo } from './styles';
 
@@ -11,6 +13,7 @@ const Header = ({logado}) => {
     <Container>
             <Logo src={logo} alt="Logo da plataforma"/>
             {logado ? (<>
+                <UserIcon src={usericon} alt="Imagem do usuário"/>
                 <Button title='Sair' />
             </>) : (<>
                 <Navigator title='Entrar' href='/login' />
