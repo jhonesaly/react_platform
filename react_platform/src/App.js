713b49ef62d68250f5699160b05c75bbd2abb0ad
Jhones <jhonesaly@gmail.com>
2023-11-23 15:31:09 -0300

    feat: login route

--- react_platform/src/App.js
+++ react_platform/src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 
 import { Home } from './pages/home';
+import { Login } from './pages/login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
       </Routes>
     </Router>
 
