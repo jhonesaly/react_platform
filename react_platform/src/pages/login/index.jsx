3f4ad0b040a86034a10161e4b391db5da58b1996
Jhones <jhonesaly@gmail.com>
2023-11-24 15:53:04 -0300

    feat: botões

--- react_platform/src/pages/login/index.jsx
+++ react_platform/src/pages/login/index.jsx
@@ -1,4 +1,6 @@
 import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+
 import { ContentContainer, Column, Title } from './styles';
 
 const Login = () => {
@@ -7,9 +9,11 @@ const Login = () => {
         <ContentContainer>
             <Column>
                 <Title>CADASTRO</Title>
+                <Button title="Enviar"/>
             </Column>
             <Column>
                 <Title>ENTRAR</Title>
+                <Button title="Enviar"/>
             </Column>
         </ContentContainer>
     </>)
