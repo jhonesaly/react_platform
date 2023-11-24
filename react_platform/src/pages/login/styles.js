661ab4675c87f5420dd54a733fca4ee433a42bf0
Jhones <jhonesaly@gmail.com>
2023-11-24 14:57:07 -0300

    feat: styles login

---
+++ react_platform/src/pages/login/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ContentContainer = styled.main`
+    display: flex;
+    flex-direction: row;
+`
+
+export const Column = styled.div`
+    flex: 1;
+`
