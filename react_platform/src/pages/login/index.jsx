50d52dc364db6f3b43cc355d83a985fd93da2525
Jhones <jhonesaly@gmail.com>
2023-11-27 09:36:08 -0300

    feat: login e senha

--- react_platform/src/pages/login/index.jsx
+++ react_platform/src/pages/login/index.jsx
@@ -7,6 +7,7 @@ import { Input } from '../../components/Input';
 import { ContentContainer, Column, Title } from './styles';
 
 import { useAuth } from '../../services/AuthContext';
+import { api } from '../../services/api';
 
 import { useForm } from 'react-hook-form';
 
@@ -17,20 +18,32 @@ const Login = () => {
     const navigate = useNavigate();
     const { handleLogin } = useAuth();
 
-    const handleClickEnviarLogin = () => {
-        navigate('/');
-        handleLogin();
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        reValidadeMode: 'onChange',
+        mode: 'onChange',
+    })
+
+    const handleClickEnviarLogin = async (formData) => {
+        try{
+            const {data} = await api.get(`/users?email=${formData.email}&senha=${formData.senha}`);
+
+            if(DataTransfer.length && data[0].id){
+                handleLogin();
+                navigate('/');
+            } else {
+                alert('Usuário ou senha inválido')
+            }
+
+            return
+        }catch(e){
+            alert('Erro: ', e)
+        }
     }
 
     const handleClickEnviarSignin = () => {
         navigate('/');
         handleLogin();
     }
-
-    const { control, handleSubmit, formState: { errors } } = useForm({
-        reValidadeMode: 'onChange',
-        mode: 'onChange',
-    })
 
     return (<>
         <Header/>
