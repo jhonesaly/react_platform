080e02a93314387eb5d921385d9b11de48604cc1
Jhones <jhonesaly@gmail.com>
2023-11-30 16:52:40 -0300

    feat: revertendo do typescript

--- react_platform/src/index.js
+++ react_platform/src/index.js
@@ -7,7 +7,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthProvider>
-    <App />
+      <App />
     </AuthProvider>
   </React.StrictMode>
 );
+
